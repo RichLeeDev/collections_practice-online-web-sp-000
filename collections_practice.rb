11b49198a7b2ce0eb6e6d96abbf49215d953aa7e
Richard Lee <richlee81192@gmail.com>
2019-09-23 00:26:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
+def sort_array_asc(array)
+  array.sort
+end 
 
+def sort_array_desc(array)
+  array.sort.reverse
+end 
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+      elsif a.length < b.length
+      -1
+      elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  nuarray = []
+  nuarray << array[0]
+  nuarray << array[2]
+  nuarray << array[1]
+  nuarray
+end
+
+def reverse_array(array)
+  array.reverse!
+end
